a20fd3545c50015751a7aaf63b4d838b8511abf7
sxavenger <sxavenger.x3@gmail.com>
2023-10-07 00:39:26 +0900

    Add: player の変数と関数

--- MyDeta/Object/Player.cpp
+++ MyDeta/Object/Player.cpp
@@ -1,1 +1,50 @@
 #include "Player.h"
+
+Player::Player() {
+	Init();
+}
+
+Player::~Player() {
+}
+
+void Player::Init() { // •Ï”‚Ì‰Šú‰»
+
+	mode_ = SEARCH;
+
+	pos_ = {
+		0.0f, // x
+		0.0f, // y
+	};
+
+	size_.radius = 20.0f;
+
+	speed_ = 1.0f;
+}
+
+void Player::Update(char* keys, char* preKeys) { /// XVˆ—
+	switch (mode_) {
+		case Mode::SEARCH:
+
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				mode_ = Mode::SHOT;
+			}
+
+
+
+			break;
+
+		case Mode::SHOT:
+
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				mode_ = Mode::SHOT;
+			}
+
+
+
+			break;
+	}
+}
+
+void Player::Draw() { /// •`‰æˆ—
+
+}
--- MyDeta/Object/Player.h
+++ MyDeta/Object/Player.h
@@ -1,4 +1,43 @@
-#pragma once
+﻿#pragma once
+#include <Novice.h>
+
+// MySource //
+#include "MyDeta/Source/Environment.h"
+#include "MyDeta/Source/MyStruct.h"
+
 class Player {
+private:
+
+	enum Mode {
+		SEARCH,
+		SHOT,
+	};
+
+	Mode mode_;
+
+	Vector2 <float>pos_; // world座標基準
+	Size <float>size_; // world座標基準
+
+	// world座標基準はカメラzoom率をかけてscreenに映すこと。
+
+	float speed_; // world座標基準
+
+	Vector2 <float>velocity_;
+	Vector2 <float>acceleration_;
+
+	// world座標のpx1とzoomした1pxは違うので注意。
+
+
+
+public:
+
+	// constructor & destructor
+	Player();
+	~Player();
+
+	// 基礎関数
+	void Init();
+	void Update(char* keys, char* preKeys);
+	void Draw();
 };
 
