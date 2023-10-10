26ec7aefaf3ceacbbbdcd10c62b90d48396aa10c
oharaAoi <k023g0108@g.neec.ac.jp>
2023-10-10 10:10:57 +0900

    zoom.hの変更(今は使わないもののコメントアウト化)

--- Zoom.cpp
+++ Zoom.cpp
@@ -18,7 +18,7 @@ Zoom::Zoom() {
 	isZoom_ = false;
 }
 
-
+//ズームする時に呼び出す初期化(今はこれを最初に呼び出すだけでいい)
 void Zoom::ZoomInInit(Vector2<float> pos) {
 	//スクロール値がプレイヤーの座標になるようにする(playerの座標がカメラ座標で0,0になる)
 	/*maxScroll_.x = pos.x - kWindowWidth / 2.0f;
@@ -36,45 +36,45 @@ void Zoom::ZoomInInit(Vector2<float> pos) {
 	scalar_ = 3.0f;
 }
 
-void Zoom::ZoomOutInit() {
-	//スクロール値を0,0にする
-	maxScroll_.x = 0;
-	maxScroll_.y = 0;
+//void Zoom::ZoomOutInit() {
+//	//スクロール値を0,0にする
+//	maxScroll_.x = 0;
+//	maxScroll_.y = 0;
+//
+//	maxScalar_ = 1.0f;
+//
+//	isZoom_ = true;
+//
+//	time_ = 0.0f;
+//}
+//
+//template <typename T>
+////倍率のズーム
+//void Zoom::ZoomEase(T& value, T zoomValue, float time) {
+//	value = std::lerp(value, zoomValue, time);
+//}
+//
+////スクロール値のズーム
+//void Zoom::ZoomEase(Vector2<float>& value, Vector2<float> zoomValue, float time) {
+//	value.x = std::lerp(value.x, zoomValue.x, time);
+//	value.y = std::lerp(value.y, zoomValue.y, time);
+//}
+//
+////flagがtrueの時これを呼び出す(大きくしていく処理)
+//void Zoom::ZoomIn() {
+//	//time_++;
+//
+//	t_ = time_ / timeLimit_;
+//
+//	ZoomEase(scalar_, maxScalar_, EaseInExpo(t_));
+//	ZoomEase(scroll_, maxScroll_, EaseInExpo(t_));
+//
+//	if (time_ > timeLimit_) {
+//		isZoom_ = false;
+//	}
+//}
 
-	maxScalar_ = 1.0f;
-
-	isZoom_ = true;
-
-	time_ = 0.0f;
-}
-
-template <typename T>
-//倍率のズーム
-void Zoom::ZoomEase(T& value, T zoomValue, float time) {
-	value = std::lerp(value, zoomValue, time);
-}
-
-//スクロール値のズーム
-void Zoom::ZoomEase(Vector2<float>& value, Vector2<float> zoomValue, float time) {
-	value.x = std::lerp(value.x, zoomValue.x, time);
-	value.y = std::lerp(value.y, zoomValue.y, time);
-}
-
-//flagがtrueの時これを呼び出す
-void Zoom::ZoomIn() {
-	//time_++;
-
-	t_ = time_ / timeLimit_;
-
-	ZoomEase(scalar_, maxScalar_, EaseInExpo(t_));
-	ZoomEase(scroll_, maxScroll_, EaseInExpo(t_));
-
-	if (time_ > timeLimit_) {
-		isZoom_ = false;
-	}
-}
-
-
+//ズーム中はこれをずっと呼び出す
 void Zoom::Update(Vector2<float> pos) {
 	if (scalar_ == 3.0f) {
 		scroll_.x = pos.x - (kWindowWidth / 2.0f);
