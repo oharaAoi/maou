87e88a36ce8a7579b68dce01a29fe97f4c0c83c7
k023g0121note <k023g0121@g.neec.ac.jp>
2023-10-19 15:36:57 +0900

    プレイヤーの死亡エフェクトを追加

---
+++ PlayerDeadParticle.cpp
@@ -0,0 +1,50 @@
+﻿#include "PlayerDeadParticle.h"
+#include <Novice.h>
+
+PlayerDeadParticle::PlayerDeadParticle(Vector2<float>pos, Vector2<float>velocity, float radius)
+{
+	pos_ = pos;
+	velocity_ = velocity;
+	radius_ = radius;
+}
+
+PlayerDeadParticle::~PlayerDeadParticle()
+{
+}
+
+void PlayerDeadParticle::Update()
+{
+	//座標を更新
+	pos_.x += velocity_.x;
+	pos_.y += velocity_.y;
+
+	//半径が0になるまで小さくする
+	if (radius_ > 0) {
+		radius_ -= 0.1f;
+	}
+
+	//透明度が0になるまで値を下げる
+	if (alpha_ > 0) {
+		alpha_ -= 10;
+	}
+	//透明度が0になったら削除
+	if (alpha_ < 0) {
+		del_ = true;
+	}
+}
+
+void PlayerDeadParticle::Draw()
+{
+	Novice::DrawEllipse(
+		static_cast<int>(pos_.x) + static_cast<int>(cie_->GetOrigine().x),
+		static_cast<int>(pos_.y) + static_cast<int>(cie_->GetOrigine().y),
+		static_cast<int>(radius_), static_cast<int>(radius_),
+		0.0f,
+		0xFFFFFF00 + alpha_, kFillModeSolid
+	);
+}
+
+bool PlayerDeadParticle::GetDelFlag()
+{
+	return del_;
+}
---
+++ PlayerDeadParticle.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <MyDeta/Source/MyStruct.h>
+#include "MyDeta/Source/Coordinate.h"
+
+class PlayerDeadParticle
+{
+public:
+	PlayerDeadParticle(Vector2<float>pos,Vector2<float>velocity,float radius); // コンストラクタ
+	~PlayerDeadParticle(); // デストラクタ
+
+	void Update(); // 更新処理
+	void Draw(); // 描画処理
+
+	bool GetDelFlag(); //パーティクルが削除されたかを取得
+
+private:
+	//インスタンス
+	Coordinate* cie_ = Coordinate::GetInstance();
+
+	Vector2<float> pos_;
+	Vector2<float> velocity_;
+	float radius_;
+	int alpha_ = 255;
+	bool del_ = false;
+};
+
