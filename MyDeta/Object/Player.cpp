ebb3d6501384e19185cbdb6eae7855df2a0d25fe
sxavenger <sxavenger.x3@gmail.com>
2023-10-22 19:39:13 +0900

    Fixed: tutorial GH

--- MyDeta/Object/Player.cpp
+++ MyDeta/Object/Player.cpp
@@ -153,47 +153,47 @@ void Player::Update(char* keys, char* preKeys, Stage& stage_) { /// 更新処理
 	//======================================================
 	//風の切り替え(OFF→弱→強 OFF)
 	switch (windowStrength_) {
-	case WindowStrength::OFF:
+		case WindowStrength::OFF:
 
-		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
-			windowStrength_ = WEAK;
-			windVolume_.x = 1.2f;
-			windVolume_.y = 1.2f;
-			stage_.SetRangeRadius(150.0f);
-			frameCountLimit_ = 10;
-		}
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				windowStrength_ = WEAK;
+				windVolume_.x = 1.2f;
+				windVolume_.y = 1.2f;
+				stage_.SetRangeRadius(150.0f);
+				frameCountLimit_ = 10;
+			}
 
-		if (overHeat_ < 100.0f) { overHeat_ += 0.5f; }
+			if (overHeat_ < 100.0f) { overHeat_ += 0.5f; }
 
-		break;
+			break;
 
-	case WindowStrength::WEAK:
+		case WindowStrength::WEAK:
 
-		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
-			windowStrength_ = STRONG;
-			windVolume_.x = 1.5f;
-			windVolume_.y = 1.5f;
-			stage_.SetRangeRadius(100.0f);
-			frameCountLimit_ = 5;
-		}
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				windowStrength_ = STRONG;
+				windVolume_.x = 1.5f;
+				windVolume_.y = 1.5f;
+				stage_.SetRangeRadius(100.0f);
+				frameCountLimit_ = 5;
+			}
 
-		if (overHeat_ > 0.0f) { overHeat_ -= 0.1f; }
+			if (overHeat_ > 0.0f) { overHeat_ -= 0.1f; }
 
-		break;
+			break;
 
-	case WindowStrength::STRONG:
+		case WindowStrength::STRONG:
 
-		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
-			windowStrength_ = OFF;
-			windVolume_.x = 0.0f;
-			windVolume_.y = 0.0f;
-			stage_.SetRangeRadius(300.0f);
-			drawLt_.x = 0.0f;
-		}
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				windowStrength_ = OFF;
+				windVolume_.x = 0.0f;
+				windVolume_.y = 0.0f;
+				stage_.SetRangeRadius(300.0f);
+				drawLt_.x = 0.0f;
+			}
 
-		if (overHeat_ > 0.0f) { overHeat_ -= 0.4f; }
+			if (overHeat_ > 0.0f) { overHeat_ -= 0.4f; }
 
-		break;
+			break;
 	}
 
 	if (overHeat_ < 0.0f) {
@@ -209,8 +209,8 @@ void Player::Draw() { /// 描画処理
 	// HP
 	DrawRhombus(
 		{ pos_.x + cie_->GetOrigine().x, pos_.y + cie_->GetOrigine().y },
-		rangeRadius_,
-		20.0f,
+		rangeRadius_ - 10.0f,
+		10.0f,
 		0x505050FF,
 		white1x1GH
 	);
@@ -218,8 +218,8 @@ void Player::Draw() { /// 描画処理
 	DrawRhombusAnimation(
 		hp_ / 15.0f,
 		{ pos_.x + cie_->GetOrigine().x, pos_.y + cie_->GetOrigine().y },
-		rangeRadius_,
-		20.0f,
+		rangeRadius_ - 10.0f,
+		10.0f,
 		ShiftColor(hp_ / 15.0f, 0xFA0000FF, 0x00FA00FF),
 		white1x1GH
 	);
@@ -258,34 +258,34 @@ void Player::Draw() { /// 描画処理
 		0xffffffff,
 		kFillModeSolid);*/
 
-	/// test guage
-	// over
+		/// test guage
+		// over
 	switch (windowStrength_) {
-	case WindowStrength::OFF:
+		case WindowStrength::OFF:
 
-		DrawRhombusAnimation(
-			overHeat_ / 100.0f,
-			{ rangePos_.x + cie_->GetOrigine().x, rangePos_.y + cie_->GetOrigine().y },
-			rangeRadius_ - 10.0f,
-			10.0f,
-			0x707070ff,
-			white1x1GH
-		);
+			DrawRhombusAnimation(
+				overHeat_ / 100.0f,
+				{ pos_.x + cie_->GetOrigine().x, pos_.y + cie_->GetOrigine().y },
+				rangeRadius_+ 5.0f,
+				10.0f,
+				0x707070ff,
+				white1x1GH
+			);
 
-		break;
+			break;
 
-	default:
+		default:
 
-		DrawRhombusAnimation(
-			overHeat_ / 100.0f,
-			{ rangePos_.x + cie_->GetOrigine().x, rangePos_.y + cie_->GetOrigine().y },
-			rangeRadius_ - 10.0f,
-			10.0f,
-			ShiftColor(overHeat_ / 100.0f, 0xFA0000FF, 0x00FA00FF),
-			white1x1GH
-		);
+			DrawRhombusAnimation(
+				overHeat_ / 100.0f,
+				{ pos_.x + cie_->GetOrigine().x, pos_.y + cie_->GetOrigine().y },
+				rangeRadius_ + 5.0f,
+				10.0f,
+				ShiftColor(overHeat_ / 100.0f, 0xFAFA00FF, 0x5f9ff7FF),
+				white1x1GH
+			);
 
-		break;
+			break;
 	}
 }
 
@@ -327,6 +327,41 @@ void Player::Update(char* keys, Stage& stage_, Restrictions restriction) {
 			windVolume_.x = 0.0f;
 			windVolume_.y = 0.0f;
 			stage_.SetRangeRadius(300.0f);
+			//描画のための処理
+			p2bDis_ = ConversionNormalizeVector(pos_, stage_.GetPos());
+
+			p2bVertical_.x = -p2bDis_.y;
+			p2bVertical_.y = p2bDis_.x;
+
+			drawTheta_ = atan2f(p2bVertical_.y, p2bVertical_.x);
+
+			//回転行列
+			rotateMatrix = MakeRotateMatrix(drawTheta_);
+
+			//平行移動
+			translateMatrix = MakeTranslateMatrix(pos_);
+
+			//行列の積
+			worldMatrix = Multiply(rotateMatrix, translateMatrix);
+
+			lt_ = Transform(originLt_, worldMatrix);
+			rt_ = Transform(originRt_, worldMatrix);
+			lb_ = Transform(originLb_, worldMatrix);
+			rb_ = Transform(originRb_, worldMatrix);
+
+
+			//画像の切り替え
+			if (windowStrength_ != OFF) {
+				frameCount_++;
+				if (frameCount_ > frameCountLimit_) {
+					drawLt_.x += 64.0f;
+					frameCount_ = 0;
+
+					if (drawLt_.x >= spriteSize.x) {
+						drawLt_.x = 0;
+					}
+				}
+			}
 
 			break;
 	}
--- main.cpp
+++ main.cpp
@@ -353,7 +353,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			/// ↓更新処理ここから
 			/// 
 
-
+			if (keys[DIK_SPACE], !preKeys[DIK_SPACE]) {
+				scene = GameScene::TITLE;
+			}
 
 			///
 			/// ↑更新処理ここまで
