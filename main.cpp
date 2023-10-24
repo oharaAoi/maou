a47f8b0c418f6abd51330d7aa3b09ec3512f5e32
k023g0121note <k023g0121@g.neec.ac.jp>
2023-10-24 11:22:37 +0900

    プレイヤー死亡エフェクトを適用

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@
 #include "MyDeta/Particle/Emitter.h"
 #include "MyDeta/Particle/Emitter2.h"
 #include "BossDeadParticle.h"
+#include "PlayerDeadEmitter.h"
 #include "PlayerWindEmitter.h"
 
 // MySceneChange //
@@ -68,6 +69,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	PlayerWindEmitter playerWindEmitter;
 
+	PlayerDeadEmitter playerDeadEmitter;
 
 	//========================================================
 	// Source
@@ -252,6 +254,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 				emitter.Update(); //エミッターの更新処理
 
+				playerDeadEmitter.Update(); //プレイヤー死亡時のエミッターの更新処理
+
+				if (player_.GetHp() <= 0 && playerDeadEmitter.GetIsGenerate() == false) { //プレイヤー死亡時にプレイヤーの位置にパーティクルを生成
+					playerDeadEmitter.PlayerDeadEmit(static_cast<int>(player_.GetPos().x), static_cast<int>(player_.GetPos().y), 16);
+				}
+				if (player_.GetHp() > 0) { 
+					playerDeadEmitter.SetIsGenerate(false);
+				}
+
 				// プレイヤーの状態によって風エフェクトの消滅までの時間、速さ、発生間隔を変更
 				switch (player_.GetWindowStrength()) {
 					case OFF:
@@ -327,6 +338,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				range_.Draw();
 				emitter.Draw(); // エミッターの描画処理を呼ぶ
 				playerWindEmitter.Draw(); // プレイヤーの風の描画処理
+				playerDeadEmitter.Draw(); // プレイヤー死亡時のパーティクルの描画処理
 
 
 				bossBullet_.Draw();
